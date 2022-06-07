938708ba95bf560716a238b045bba5188b3b782b
ItzelPC14 <utp00075@alumno.utpuebla.edu.mx>
2022-06-07 11:56:41 -0500

    Agrego texto y boton

--- app/src/main/java/com/example/resstapp/MainActivity.java
+++ app/src/main/java/com/example/resstapp/MainActivity.java
@@ -3,21 +3,30 @@ package com.example.resstapp;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
-
+import java.util.List;
 import com.example.resstapp.databinding.ActivityMainBinding;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends Activity {
 
     private TextView mTextView;
-    private ActivityMainBinding binding;
+    private Button mButton;
+    private  final List<String> mensajes= Arrays.asList(
+            "Hola",
+            "Como estas?",
+            "Bye"
+    );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        mTextView=findViewById(R.id.mensajetxtview);
+        mButton=findViewById(R.id.botonbtn);
 
-        mTextView = binding.text;
+        mTextView,setText(mensajes.get(1));
     }
 }
